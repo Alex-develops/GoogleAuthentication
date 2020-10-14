0bcf39d9b84653060e6a748fd567b8c083143283
Alex <alex_s11p@abv.bg>
2020-10-14 20:21:56 +0100

    Level 3 Hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const ejs=require("ejs");
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const encrypt= require("mongoose-encryption");
+
+const md5=require("md5");
 const app=express();
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true} );
 
@@ -13,7 +14,7 @@ const userSchema= new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 const User= new mongoose.model("User", userSchema);
 
@@ -36,8 +37,8 @@ app.get("/register", function(req,res){
 
 app.post("/register", function(req, res){
   const newUser= new User({
-    email: req.body.userName,
-    password:req.body.password
+    email: req.body.username,
+    password:md5(req.body.password)
   });
 
 newUser.save(function(err){
@@ -51,8 +52,8 @@ newUser.save(function(err){
 
 
 app.post("/login", function(req, res){
-  const username=req.body.userName;
-  const password=req.body.password;
+  const username=req.body.username;
+  const password=md5(req.body.password);
 
   User.findOne({email:username}, function(err, foundUser){
     if (err){
